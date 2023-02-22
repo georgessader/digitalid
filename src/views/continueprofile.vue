3c57676aebba7c9881034248cf358f682255ab1f
georgessader <georges.sader.93@hotmail.com>
2023-02-22 15:23:04 +0200

    m

--- src/views/continueprofile.vue
+++ src/views/continueprofile.vue
@@ -29,9 +29,32 @@
               <v-text-field v-model="gpa" label="GPA"></v-text-field>
               <v-text-field v-model="creditcompleted" label="Credits Completed"></v-text-field>
 
-              <v-btn type="submit" block class="mt-2" color="success" @click="register">Submit</v-btn>
+              <v-btn type="submit" block class="mt-2" color="success" @click="addEducation">Submit</v-btn>
             </v-form>
           </v-sheet>
+
+          <!-- display -->
+          <h1 style="text-align: center; margin-top: 40px;">My Educations</h1>
+          <v-sheet width="400" class="mx-auto">
+            <v-form @submit.prevent v-for="(e, i) in educations['detail']">
+
+              <v-radio-group v-model="e['type']">
+                <v-radio label="School" value="1"></v-radio>
+                <v-radio label="University" value="2"></v-radio>
+              </v-radio-group>
+
+              <v-text-field v-model="e['college_name']" label="College Name"></v-text-field>
+              <v-text-field v-model="e['major']" label="Major"></v-text-field>
+              <v-text-field v-model="e['gpa']" label="GPA"></v-text-field>
+              <v-text-field v-model="e['credits_completed']" label="Credits Completed"></v-text-field>
+              <v-btn type="submit" block class="mt-2" color="success" @click="editEducation(e['id'],e['type'], e['college_name'],e['major'],e['gpa'],e['credits_completed'])">Edit</v-btn>
+              <v-btn type="submit" block class="mt-2" color="delete" @click="deleteEducation(e['id'])">Delete</v-btn>
+            </v-form>
+          </v-sheet>
+
+
+          
+
         </v-window-item>
 
 
@@ -49,13 +72,8 @@
         <v-window-item>
           <v-sheet width="400" class="mx-auto">
             <v-form @submit.prevent>
-              <v-file-input label="Health report"></v-file-input>
-              <v-file-input label="Chronic diseases"></v-file-input>
-              <v-file-input label="Allergy"></v-file-input>
-              <v-file-input label="Vaccination report"></v-file-input>
-              <v-file-input label="NSSF number (if available)"></v-file-input>
-              <v-file-input label="Insurance Docs (if available)"></v-file-input>
-              <v-btn type="submit" block class="mt-2">Submit</v-btn>
+            
+            <v-btn type="submit" block class="mt-2">Submit</v-btn>
             </v-form>
           </v-sheet>
         </v-window-item>
@@ -70,7 +88,7 @@
 
 
         <v-window-item>
-          <v-sheet width="400" class="mx-auto">
+          <!-- <v-sheet width="400" class="mx-auto">
             <v-form @submit.prevent>
               <v-text-field v-model="major" label="Major"></v-text-field>
               <v-checkbox v-model="prevexp" label="Do you have previous experience?"></v-checkbox>
@@ -92,7 +110,7 @@
               <v-text-field v-model="linkedinprofile" label="Linked In Profile"></v-text-field>
               <v-btn type="submit" block class="mt-2">Submit</v-btn>
             </v-form>
-          </v-sheet>
+          </v-sheet> -->
         </v-window-item>
       </v-window>
     <h4 style="color:green;">
@@ -113,16 +131,17 @@ export default {
       status:"",
 
       ctype:"",
-      collname:"Col name",
-      major:"CCE",
-      gpa:"3.9",
-      creditcompleted:"105",
+      collname:"",
+      major:"",
+      gpa:"",
+      creditcompleted:"",
+      educations:[],
 
     }
   },
 
   methods: {
-    async register() {
+    async addEducation() {
       try {
         let st = {
           "user": sessionStorage.getItem('email'),
@@ -135,18 +154,65 @@ export default {
         const response = await this.$http.post('http://localhost:8000/educations/create', st);
         if (response.status == 200) {
           this.status="Success!"
+          location.reload();
         }
         console.log(response.status);
       } catch (error) {
         this.status = "Error"
         console.log(error);
       }
-    }
+    },
+    async getEducations() {
+      try {
+        const response = await this.$http.get('http://localhost:8000/educations/'+sessionStorage.getItem('email'));
+        if (response.status == 200) {
+          this.educations=response.data;
+          console.log(this.educations.values)
+        }
+      } catch (error) {
+        this.status = "Error"
+        console.log(error);
+      }
+    },
+    async editEducation(id,type,colname,major,gpa,credits) {
+      try {
+        let st = {
+          "type": type,
+          "major": major,
+          "credits_completed": credits,
+          "gpa": gpa,
+          "college_name": colname
+        }
+        const response = await this.$http.patch('http://localhost:8000/educations/'+id, st);
+        if (response.status == 200) {
+          this.status="Success!"
+          location.reload();
+        }
+        console.log(response.status);
+      } catch (error) {
+        this.status = "Error"
+        console.log(error);
+      }
+    },
+    async deleteEducation(id) {
+      try {
+        const response = await this.$http.delete('http://localhost:8000/educations/'+id);
+        if (response.status == 200) {
+          this.status="Deleted!"
+          location.reload();
+        }
+        console.log(response.status);
+      } catch (error) {
+        this.status = "Error"
+        console.log(error);
+      }
+    },
   },
   created() {
     if (!sessionStorage.getItem('id'))
       this.$router.push({ path: '/' })
     this.$root.$emit('loadApp')
+    this.getEducations()
   }
 };
 </script>
