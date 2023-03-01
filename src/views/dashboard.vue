6bfb1e73bb09d6aae9e8f39e05252c4eca25dfae
georgessader <georges.sader.93@hotmail.com>
2023-03-01 16:34:47 +0200

    d

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,6 +1,15 @@
 <template>
-  <!-- <iframe width="1000" height="1000" src="C:\Users\User\digitalid\digitalid-backend\uploads\education\69109fb9-b661-4a17-ac35-9e4a55e0bc44\SV_Math_2021_2_En.pdf"></iframe> -->
-  <a href="C:\Users\User\digitalid\digitalid-backend\uploads\education\69109fb9-b661-4a17-ac35-9e4a55e0bc44\SV_Math_2021_2_En.pdf">see</a>
+  <v-sheet width="500" class="mx-auto">
+    <v-form @submit.prevent>
+      <v-radio-group v-model="dtype">
+        <v-radio label="Doc 1" value="1"></v-radio>
+        <v-radio label="Doc 2" value="2"></v-radio>
+      </v-radio-group>
+      <v-text-field v-model="expdate" label="Expectation Date" type="date"></v-text-field>
+      <v-textarea label="Label" variant="outlined" v-model="comment"></v-textarea>
+      <v-btn block class="mt-2" color="success" @click="sendreq">Send Request</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
 
 <script>
@@ -8,16 +17,23 @@
 export default {
   data() {
     return {
-      infos: "",
-      show: false,
+      expdate: "",
+      dtype:"",
+      comment:"",
     }
   },
 
   methods: {
-    async getinfos() {
+    async sendreq() {
       try {
+        let st={
+          "user":sessionStorage.getItem('id'),
+          "expected_date":this.expdate,
+          "document_type":this.dtype,
+          "comment":this.comment
+        }
         console.log(sessionStorage.getItem('id'))
-        const response = await this.$http.get('http://localhost:8000/users/details/' + sessionStorage.getItem('id'));
+        const response = await this.$http.post('http://localhost:8000/docs/create',st);
         this.infos = response.data;
         console.log(response.data)
       } catch (error) {
@@ -25,12 +41,6 @@ export default {
         console.log(error);
       }
     }
-  },
-  created() {
-    if (!sessionStorage.getItem('id'))
-      this.$router.push({ path: '/' })
-    this.$root.$emit('loadApp')
-    this.getinfos()
   }
 };
 </script>
